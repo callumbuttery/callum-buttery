5c8d81c505adccc990f3972ef73b7509284e0281
callum buttery <callumbuttery@icloud.com>
2023-01-03 19:08:36 +0000

    Fix: move flex into component and off of import

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -3,7 +3,7 @@ import picture from '../images/picture.jpeg'
 function profilePicture() {
     return (
 
-        <div>
+        <div className='flex justify-center pt-52'>
             <img class="w-96 h-96 rounded-full ring-4 ring-white dark:ring-white" src={picture} alt="Bordered avatar"/>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import ProfilePic from '../components/ProfilePic'
 function Home() {
     return (
         <div className="h-screen bg-[#5bc0be]">
-            <div className='flex justify-center pt-52'>
+            <div>
                 <ProfilePic />
             </div>
             <h1 className="text-5xl font-bold underline text-white flex justify-center pt-16">
