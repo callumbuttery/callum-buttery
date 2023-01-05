575fd0379716b067bcbe4789ec1ca45c7ac0d2ee
callum buttery <callumbuttery@icloud.com>
2023-01-05 23:57:20 +0000

    Fix: change top padding on text

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -4,7 +4,7 @@ export default function WelcomeText() {
             <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
                 CALLUM BUTTERY
             </div>
-            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest max-sm:2-xl max-sm:mt-0">
+            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest max-sm:2-xl max-sm:mt-2">
                 Software Engineer
             </div>
         </div>
