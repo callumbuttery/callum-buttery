9b733d360d92c5b2ae64aefe5b7568eb0cf6f620
callum buttery <callumbuttery@icloud.com>
2023-01-05 23:48:56 +0000

    Fix: nav now clickable

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,7 +1,7 @@
 export default function WelcomeText() {
     return (
-        <div>
-            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center pt-96 text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-2xl max-sm:tracking-[.01em]">
+        <div className="h-screen place-items-center pt-96">
+            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-2xl max-sm:tracking-[.01em]">
                 CALLUM BUTTERY
             </div>
             <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest drop-shadow-4xl max-sm:text-base">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,6 @@ import WelcomeText from '../components/WelcomeText';
 function Home() {
     return (
         <div className="h-screen bg-[url('images/ae.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
-            {/* <div>
-                <ProfilePic />
-            </div>
-            <div>   
-                <DetailsCard />
-            </div> */}
             <WelcomeText />
         </div>
     )
