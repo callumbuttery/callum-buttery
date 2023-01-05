4b80e654a75b336202a86a10f0f501d239c3f643
callum buttery <callumbuttery@icloud.com>
2023-01-05 23:52:45 +0000

    Fix: increase text sizes for mobile

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,10 +1,10 @@
 export default function WelcomeText() {
     return (
         <div className="h-screen place-items-center pt-96">
-            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-2xl max-sm:tracking-[.01em]">
+            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
                 CALLUM BUTTERY
             </div>
-            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest drop-shadow-4xl max-sm:text-base">
+            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest drop-shadow-4xl max-sm:2-xl">
                 Software Engineer
             </div>
         </div>
