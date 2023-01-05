a7538f1320d74f1ea97ca616c72f90d60cfa3eec
callum buttery <callumbuttery@icloud.com>
2023-01-05 23:55:44 +0000

    Guess: mobile font fix

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -4,7 +4,7 @@ export default function WelcomeText() {
             <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
                 CALLUM BUTTERY
             </div>
-            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest drop-shadow-4xl max-sm:2-xl">
+            <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700" className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest max-sm:2-xl max-sm:mt-0">
                 Software Engineer
             </div>
         </div>
