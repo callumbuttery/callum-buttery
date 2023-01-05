733716b9c256d9fdd50bc406ea772918c18d728a
callum buttery <callumbuttery@icloud.com>
2023-01-05 23:16:47 +0000

    Feat: responsiveness

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,10 +1,10 @@
 export default function WelcomeText() {
     return (
         <div>
-            <div className="flex justify-center text-center pt-96 text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto">
+            <div className="flex justify-center text-center pt-96 text-8xl tracking-[.15em] text-white font-header font-bold w-4/6 mx-auto max-sm:text-2xl max-sm:tracking-[.01em]">
                 CALLUM BUTTERY
             </div>
-            <div className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest drop-shadow-4xl">
+            <div className="flex justify-center text-center text-2xl text-white font-light mt-5 tracking-widest drop-shadow-4xl max-sm:text-base">
                 Software Engineer
             </div>
         </div>
