579613594b3ff08aaec1bd60ebac8104300e9a22
callum buttery <callumbuttery@icloud.com>
2023-01-07 13:25:17 +0000

    Feat: add in dumby project components

---
+++ src/components/ProjectCallumButtery.js
@@ -0,0 +1,7 @@
+export default function ProjectCallumButtery() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,6 @@ import ProjectDoodleNoodles from "../components/ProjectDoodleNoodles"
 export default function Projects() {
     return (
         <div className='h-screen bg-gradient-to-t from-teal-200 to-teal-600'>
-            this is Projects
             <ProjectDoodleNoodles/>
         </div>
     )
