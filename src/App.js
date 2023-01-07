22f49de913c50223f5348a325169b0637dbf2d63
callum buttery <callumbuttery@icloud.com>
2023-01-07 12:14:56 +0000

    Feat: add in router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Projects from './pages/Projects.js';
 import NavBar from './components/Navbar';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   useEffect(() => {
     AOS.init();
   }, [])
-  
+
   return (
     <div>
-      <NavBar />
-      <Home />
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/projects" element={<Projects />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
+import { BrowserRouter as Router, Link as RouterLink } from 'react-router-dom';
+
 export default function NavBar() {
     return (
         <div data-aos="fade-down" data-aos-duration="2000" data-aos-delay="">
             <nav className="bg-transparent h-24 px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-transparent">
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     <div className="flex mx-auto justify-center w-full">
-                        <ul className="pt-7 flex p-4 mt-4 md:flex-row md:space-x-20 md:mt-0 md:text-sm md:font-medium ">
-                            <li>
-                                <a href="#" className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0" aria-current="page">About</a>
-                            </li>
-                            <li>
-                                <a href="#" className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0">Projects</a>
-                            </li>
-                            <li>
-                                <a href="#" className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0">CV</a>
-                            </li>
-                        </ul>
+                        <div className="pt-7 flex p-4 mt-4 md:flex-row md:space-x-20 md:mt-0 md:text-sm md:font-medium">
+                            <RouterLink className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0" to='/'>Home</RouterLink>
+
+                            <RouterLink className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0" to='/about'>About</RouterLink>
+
+                            <RouterLink className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0" to='/projects'>Projects</RouterLink>
+
+                            <RouterLink className="text-lg block py-2 pl-3 pr-3 text-white hover:text-[#ffd60a] tracking-[.15em] font-header hover:bg-[#ffd60a] md:hover:bg-transparent md:p-0" to='/cv'>CV</RouterLink>
+                        </div>
                     </div>
                 </div>
             </nav>
