335b3cc05217ae4fa0be9aae7338cd93755940bb
callum buttery <callumbuttery@icloud.com>
2023-01-14 16:45:13 +0000

    Fix: change hover text colour on nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,13 @@ export default function NavBar() {
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
                     <div className="flex mx-auto justify-center w-full">
                         <div className="pt-7 flex p-4 mt-4 drop-shadow-4xl  md:flex-row md:space-x-20 md:mt-0 md:text-sm md:font-medium">
-                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#000000] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/'>Home</RouterLink>
+                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#FF5733] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/'>Home</RouterLink>
 
-                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#000000] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/about'>About</RouterLink>
+                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#FF5733] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/about'>About</RouterLink>
 
-                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#000000] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/projects'>Projects</RouterLink>
+                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#FF5733] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/projects'>Projects</RouterLink>
 
-                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#000000] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/cv'>CV</RouterLink>
+                            <RouterLink className="text-xl block py-2 pl-3 pr-3 text-white drop-shadow-4xl hover:text-[#FF5733] hover:scale-110 hover:duration-300 tracking-[.15em] font-header md:hover:bg-transparent md:p-0" to='/cv'>CV</RouterLink>
                         </div>
                     </div>
                 </div>
