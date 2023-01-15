cad7976bf62891d871df1b98e9a1c90703d24034
callum buttery <callumbuttery@icloud.com>
2023-01-15 14:40:32 +0000

    Feat: add in components onto about page

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -3,8 +3,8 @@ import picture from '../images/picture.jpeg'
 export default function profilePicture() {
 
     return (
-        <div className='flex justify-center pt-64 drop-shadow-4xl' data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700">
-            <img className="w-96 h-96 rounded-full ring-4 ring-white dark:ring-white" src={picture} alt="Bordered avatar" />
+        <div className='flex justify-center drop-shadow-4xl' data-aos="fade-down" data-aos-duration="2000" data-aos-delay="700">
+            <img className="w-72 h-72 rounded-full ring-2 ring-white" src={picture} alt="Bordered avatar" />
         </div>
     )
 }
