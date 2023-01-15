68d18c7dc9a2a48c555107769e1ebdd77c0426f8
callum buttery <callumbuttery@icloud.com>
2023-01-15 19:00:09 +0000

    Feat: add in template timeline on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import ProfilePic from "../components/ProfilePic"
 import DetailsCard from "../components/DetailsCard"
+import Timeline from "../components/Timeline"
 
 export default function About() {
     return (
-        <div className='h-screen pt-40 pb-10 place-items-center bg-gradient-to-b from-[#00a0c2] via-[#c48f95] to-[#e06f43] max-sm:h-full'>
+        <div className='h-full pt-40 pb-10 place-items-center bg-gradient-to-b from-[#00a0c2] via-[#c48f95] to-[#e06f43] max-sm:h-full'>
             <ProfilePic />
             <DetailsCard/>
+            <Timeline/>
         </div>
     )
 }
