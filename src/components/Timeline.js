a220fc8f39d7b4be6b95ac1b86d42ecf0c1ba197
callum buttery <callumbuttery@icloud.com>
2023-01-21 17:42:04 +0000

    Feat: complete timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,7 +2,7 @@ export default function Timeline() {
     return (
         <section className="pt-32">
             <div className="container max-w-5xl px-4 py-12 mx-auto">
-                <div className="grid gap-4 mx-4 sm:grid-cols-12">
+                <div className="grid gap-10 mx-4 sm:grid-cols-12">
                     <div className="col-span-12 sm:col-span-3">
                         <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-[#00a0c2]">
                             <h3 className="text-3xl font-semibold text-white">History</h3>
@@ -11,20 +11,25 @@ export default function Timeline() {
                     </div>
                     <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
                         <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-white">
-                            <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
-                                <h3 className="text-xl text-white font-semibold tracking-wide">Donec porta enim vel </h3>
-                                <time className="text-xs tracking-wide uppercase dark:text-gray-400">Dec 2020</time>
-                                <p className="mt-3">Pellentesque feugiat ante at nisl efficitur, in mollis orci scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+                            <div data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500" className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
+                                <h3 className="text-xl text-white font-semibold tracking-wide">Raytheon UK</h3>
+                                <time className="text-xs tracking-wide uppercase">Dec 2022 - Current</time>
+                                <p className="mt-1">Full Stack Software Engineer</p>
                             </div>
-                            <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
-                                <h3 className="text-xl text-white font-semibold tracking-wide">Aliquam sit amet nunc ut</h3>
-                                <time className="text-xs tracking-wide uppercase dark:text-gray-400">Jul 2019</time>
-                                <p className="mt-3">Morbi vulputate aliquam libero non dictum. Aliquam sit amet nunc ut diam aliquet tincidunt nec nec dui. Donec mollis turpis eget egestas sodales.</p>
+                            <div data-aos="fade-down" data-aos-duration="1000" data-aos-delay="1000" className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
+                                <h3 className="text-xl text-white font-semibold tracking-wide">Odro LTD</h3>
+                                <time className="text-xs tracking-wide uppercase">Jan 2021 - Dec 2022</time>
+                                <p className="mt-1">Junior software engineer - Working with Vue.js / JS / Node.js / MongoDB</p>
                             </div>
-                            <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
-                                <h3 className="text-xl text-white font-semibold tracking-wide">Pellentesque habitant morbi</h3>
-                                <time className="text-xs tracking-wide uppercase dark:text-gray-400">Jan 2016</time>
-                                <p className="mt-3">Suspendisse tincidunt, arcu nec faucibus efficitur, justo velit consectetur nisl, sit amet condimentum lacus orci nec purus. Mauris quis quam suscipit, vehicula felis id, vehicula enim.</p>
+                            <div data-aos="fade-down" data-aos-duration="1000" data-aos-delay="1500" className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
+                                <h3 className="text-xl text-white font-semibold tracking-wide">Edinburgh Napier University</h3>
+                                <time className="text-xs tracking-wide uppercase">Jun 2018 - May 2021</time>
+                                <p className="mt-1">BEng Software Engineering - Graduated with 1st Class Honours</p>
+                            </div>
+                            <div data-aos="fade-down" data-aos-duration="1000" data-aos-delay="2000" className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#00a0c2]">
+                                <h3 className="text-xl text-white font-semibold tracking-wide">Glasgow Clyde College</h3>
+                                <time className="text-xs tracking-wide uppercase">Jan 2018 - Jun 2018</time>
+                                <p className="mt-1">HNC Computing - Graduated with an A</p>
                             </div>
                         </div>
                     </div>
