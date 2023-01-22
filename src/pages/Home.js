cdf8c7e2f866e35c866eaa08fa1f737eacf19c7f
callum buttery <callumbuttery@icloud.com>
2023-01-22 13:38:01 +0000

    Feat: change jpg to webp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import WelcomeText from '../components/WelcomeText';
 
 function Home() {
     return (
-        <div className="h-screen bg-[url('images/ae.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+        <div className="h-screen bg-[url('images/ae.webp')] bg-no-repeat bg-cover bg-center bg-fixed">
             <WelcomeText />
         </div>
     )
