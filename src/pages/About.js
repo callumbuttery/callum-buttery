44dff02ebb3bd51da2d167f09fb36ca70a95bbb3
callum buttery <callumbuttery@icloud.com>
2023-04-03 21:44:26 +0100

    Feat: update about UI

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,19 @@ import ScrollAnimator from "../components/scrollAnimator"
 
 export default function About() {
     return (
-        <div className='h-full pt-40 pb-10 place-items-center bg-gradient-to-b from-[#00a0c2] via-[#c48f95] to-[#e06f43] max-sm:h-full'>
-            <ProfilePic />
-            <DetailsCard/>
-            <ScrollAnimator/>
-            <Timeline/>
+        <div className='place-items-center max-sm:h-full'>
+            <div className="h-full bg-[url('images/aboutwallpaper.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+                <div className="pt-48 pb-48">
+                    <ProfilePic />
+                    <DetailsCard />
+                </div>
+            </div>
+            <div className="h-full bg-[url('images/aboutwallpaper2.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+                <div className="pt-48 pb-32">
+                    <ScrollAnimator />
+                    <Timeline />
+                </div>
+            </div>
         </div>
     )
 }
