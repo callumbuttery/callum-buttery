9a84aa61c09199f49beea205343f9b913670b568
callum buttery <callumbuttery@icloud.com>
2023-04-09 11:49:54 +0100

    Feat: add ability to download / open CV

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,9 +1,15 @@
+import cv from '../files/cv.pdf'
+
 export default function CV() {
     return (
-        <div className='h-screen pt-40 pb-10 place-items-center bg-gradient-to-b from-[#00a0c2] via-[#c48f95] to-[#e06f43] max-sm:h-fill'>
-            <button href='' download className="mt-52 animate-pulse bg-transparent py-2 px-3 pl-6 text-center border border-white flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
-                Available soon
+        <div className='h-screen pt-32 pb-10 place-items-center bg-gradient-to-b from-[#00a0c2] via-[#c48f95] to-[#e06f43] max-sm:h-fill'>
+            <button href='' onClick={() => window.open(cv)} className="mt-52 max-w-32 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
+                Open in tab
             </button>
+            <span className="mt-16 max-w-32 bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">OR</span>
+            <a download href={cv} className="mt-16 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
+                Download
+            </a>
         </div>
     )
 }
