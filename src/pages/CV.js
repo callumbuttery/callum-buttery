107a079b5a9e9b388c5d30f043f37b3e3f2c6bbb
callum buttery <callumbuttery@icloud.com>
2023-04-09 12:15:36 +0100

    Fix: restyle cv page

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -2,14 +2,16 @@ import cv from '../files/cv.pdf'
 
 export default function CV() {
     return (
-        <div className='h-screen pt-32 pb-10 place-items-center bg-gradient-to-b from-[#00a0c2] via-[#c48f95] to-[#e06f43] max-sm:h-fill'>
-            <button href='' onClick={() => window.open(cv)} className="mt-52 max-w-32 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
-                Open in tab
-            </button>
-            <span className="mt-16 max-w-32 bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">OR</span>
-            <a download href={cv} className="mt-16 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">
-                Download
-            </a>
+        <div className="h-screen pt-32 pb-10 place-items-center h-screen bg-[url('images/career.jpg')] bg-no-repeat bg-cover bg-center bg-fixed">
+            <div className='container max-w-3xl mx-auto backdrop-blur-md mb-4 mt-4'>
+                <button href='' onClick={() => window.open(cv)} className="mt-52 max-w-32 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-5xl tracking-[.35em] text-red-500 font-header font-bold  mx-auto max-sm:text-4xl max-sm:tracking-[.1em]">
+                    Open in tab
+                </button>
+                <span className="mt-16 max-w-32 bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">OR</span>
+                <a download href={cv} className="mt-16 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-5xl tracking-[.35em] text-red-500 font-header font-bold  mx-auto max-sm:text-4xl max-sm:tracking-[.1em]">
+                    Download
+                </a>
+            </div>
         </div>
     )
 }
