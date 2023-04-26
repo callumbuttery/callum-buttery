8de10bf988deeb6bb9f20942018f5426d3c2bfed
Callum Buttery <callumbuttery@icloud.com>
2023-04-26 14:49:14 +0100

    Fix: revert change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       dropShadow: {
