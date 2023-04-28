668b6c8394331a9a2b5d12dace7c1fe33005cfb8
Callum Buttery <callumbuttery@icloud.com>
2023-04-28 10:58:27 +0100

    Fix: update colour of text

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -4,11 +4,11 @@ export default function CV() {
     return (
         <div className="h-screen pt-32 pb-10 place-items-center h-screen bg-gradient-to-b from-black to-red-500">
             <div className='container max-w-3xl mx-auto'>
-                <button href='' onClick={() => window.open(cv)} className="mt-52 max-w-32 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-5xl tracking-[.35em] text-sky-500 font-header font-bold  mx-auto max-sm:text-4xl max-sm:tracking-[.1em]">
+                <button href='' onClick={() => window.open(cv)} className="mt-52 max-w-32 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-5xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-4xl max-sm:tracking-[.1em]">
                     Open in tab
                 </button>
                 <span className="mt-16 max-w-32 bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-4xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-3xl max-sm:tracking-[.1em]">OR</span>
-                <a download href={cv} className="mt-16 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-5xl tracking-[.35em] text-sky-500 font-header font-bold  mx-auto max-sm:text-4xl max-sm:tracking-[.1em]">
+                <a download href={cv} className="mt-16 animate-pulse bg-transparent py-2 px-3 pl-6 text-center flex justify-center text-center text-5xl tracking-[.35em] text-white font-header font-bold  mx-auto max-sm:text-4xl max-sm:tracking-[.1em]">
                     Download
                 </a>
             </div>
