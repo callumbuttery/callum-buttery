2412d94530726d244e8f39f01a0a38d0a4b69188
Callum Buttery <callumbuttery@icloud.com>
2023-04-28 11:07:03 +0100

    Fix: reduce tech image widths as they were calling x-axis overflow

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -11,15 +11,15 @@ import python from '../images/python.png'
 export default function Tech() {
     return (
         <div className="place-items-center pt-16 pb-32 max-w-fit flex-wrap mx-auto flex justify-center max-sm:inline-block">
-            <a href="https://www.javascript.com/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-96 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={JS} alt='Javascript' /></a>
-            <a href="https://www.typescriptlang.org/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-96 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={TS} alt='Typescript' /></a>
-            <a href="https://vuejs.org/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-96 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={vue} alt='Vue.js' /></a>
-            <a href="https://reactjs.org/" rel='noreferrer' target='blank'><img className="animate-spin-slow object-contain h-48 w-96 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={React} alt='React.js' /></a>
-            <a href="https://nodejs.org/en/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-96 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={node} alt='Node.js' /></a>
-            <a href="https://tailwindcss.com/" rel='noreferrer' target='blank'><img className="object-contain h-32 w-72 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300 " src={tailwind} alt='Tailwindcss' /></a>
-            <a href="https://www.mongodb.com/" rel="noreferrer" target="blank"><img className="object-contain h-48 w-96 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={mongodb} alt='MongoDB' /></a>
-            <a href="https://aws.amazon.com" rel="noreferrer" target="blank"><img className="object-contain h-48 w-96 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={aws} alt='Aws S3' /></a>
-            <a href="https://www.python.org/" rel="noreferrer" target="blank"><img className="object-contain h-48 w-96 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={python} alt='Python' /></a>
+            <a href="https://www.javascript.com/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-64 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={JS} alt='Javascript' /></a>
+            <a href="https://www.typescriptlang.org/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-64 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={TS} alt='Typescript' /></a>
+            <a href="https://vuejs.org/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-64 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={vue} alt='Vue.js' /></a>
+            <a href="https://reactjs.org/" rel='noreferrer' target='blank'><img className="animate-spin-slow object-contain h-48 w-64 ml-10 mt-10 drop-shadow-4xl hover:scale-110 hover:duration-300" src={React} alt='React.js' /></a>
+            <a href="https://nodejs.org/en/" rel='noreferrer' target='blank'><img className="object-contain h-48 w-64 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={node} alt='Node.js' /></a>
+            <a href="https://tailwindcss.com/" rel='noreferrer' target='blank'><img className="object-contain h-32 w-64 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300 " src={tailwind} alt='Tailwindcss' /></a>
+            <a href="https://www.mongodb.com/" rel="noreferrer" target="blank"><img className="object-contain h-48 w-64 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={mongodb} alt='MongoDB' /></a>
+            <a href="https://aws.amazon.com" rel="noreferrer" target="blank"><img className="object-contain h-48 w-64 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={aws} alt='Aws S3' /></a>
+            <a href="https://www.python.org/" rel="noreferrer" target="blank"><img className="object-contain h-48 w-64 ml-10 mt-20 drop-shadow-4xl hover:scale-110 hover:duration-300" src={python} alt='Python' /></a>
         </div>
     )
 }
